a3ded2298e83a1e9fe4dd251eff2a8d139507028
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-09-29 21:56:29 -0500

    some get routes working, one post working

--- server.js
+++ server.js
@@ -2,14 +2,44 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const { readFromFile, readAndAppend } = require('./helper/fsUtils')
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>
-res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/notes', (req, res)=>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+app.get('/', (req, res) => {
+    readFromFile('/public/index.html').then((data) => res.json(JSON.parse(data)))
+});
+
+app.get('/api/notes', (req, res)=> {
+    readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
+});
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    // console.log(req.body);
+    if ( title && text ){
+        const newNote = {
+            title,
+            text,
+        };
+        fs.readAndAppend(newNote, './db/feedback.json');
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+        // console.log(response);
+        res.json(response);
+        // console.log(response);
+    } else {
+        res.json('Error in posting note')
+    }
+});
 
 app.listen(PORT, ()=>
 console.log(`App listening at http://localhost:${PORT}`)
