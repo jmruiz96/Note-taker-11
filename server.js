4f72ed3fa485997338b51ee0ac63aa5aee220f10
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-09-29 23:39:59 -0500

    wokring on delete route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const { readFromFile, readAndAppend } = require('./helper/fsUtils')
+const { readFromFile, writeToFile, readAndAppend } = require('./helper/fsUtils')
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
@@ -29,6 +29,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
+            id: uuidv4(),
         };
         readAndAppend(newNote, './db/db.json');
         const response = {
@@ -43,6 +44,20 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+// app.delete('/api/notes/:id', (req, res) => {
+// const { title, text, noteId} = req.body;
+// if (title && text && noteId){
+    
+// }
+// }
+// this was not working- scrapped 
+app.delete('/api/notes/:id', (req, res) => {
+    noteId = JSON.parse(req.params.id);
+    readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)));
+    notes.JSONparse(notes)
+    notes = notes.filter(val => val.id !== noteId)
+    writeToFile('./db/db.json').then((data)=> res.json(JSON.parse(data)));
+  })
 
 
 app.listen(PORT, ()=>
